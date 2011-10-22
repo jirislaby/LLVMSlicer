d3d7b25d5f24193392b69f2a00d37f44848c6058
Jiri Slaby <jirislaby@gmail.com>
2011-10-22 15:14:43 +0200

    slic: fix StaticSlicer header
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- src/Slicing/StaticSlicer.cpp
+++ src/Slicing/StaticSlicer.cpp
@@ -1,3 +1,10 @@
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+
+#include "llvm/Instructions.h"
+#include "llvm/Function.h"
+#include "llvm/Value.h"
+
 #include "StaticSlicer.h"
 
 namespace llvm { namespace slicing { namespace detail {
