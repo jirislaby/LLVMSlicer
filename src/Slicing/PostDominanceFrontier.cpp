8a9cdba974d1deb048b767c63bdb6760e1abe161
Jiri Slaby <jirislaby@gmail.com>
2012-09-30 23:54:01 +0200

    Slicing: add control dependence graph computation
    It is unused though. It is an implemenattion of algorithm from
    Muchnick. I thought I would use it isntead of postd dom frontiers, but
    it is broken in the same way. Per definition, post-doms suck for
    infinite loops.
    
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- src/Slicing/PostDominanceFrontier.cpp
+++ src/Slicing/PostDominanceFrontier.cpp
@@ -17,6 +17,66 @@ using namespace llvm;
 static RegisterPass<PostDominanceFrontier> X("postdom-frontier", "Computes postdom frontiers");
 char PostDominanceFrontier::ID = 0;
 
+#ifdef CONTROL_DEPENDENCE_GRAPH
+void PostDominanceFrontier::constructS(const PostDominatorTree &DT,
+		Function &F, Stype &S) {
+  for (Function::iterator I = F.begin(), E = F.end(); I != E; ++I) {
+    BasicBlock *m = I;
+    DomTreeNode *mNode = DT[m];
+    for (succ_iterator II = succ_begin(m), EE = succ_end(m); II != EE; ++II) {
+      BasicBlock *n = *II;
+      DomTreeNode *nNode = DT[n];
+      if (!DT.properlyDominates(nNode, mNode))
+	S.insert(std::make_pair(mNode, nNode));
+    }
+  }
+}
+
+/*
+ * Taken from Dominators.h.
+ * Changed to return a path from LCA to B and optimized.
+ */
+const DomTreeNode *
+PostDominanceFrontier::findNearestCommonDominator(const PostDominatorTree &DT,
+		DomTreeNode *A, DomTreeNode *B) {
+  BasicBlock *BB = A->getBlock();
+  assert(BB);
+  Frontiers[B->getBlock()].insert(BB);
+
+  // If A dominates B then A is nearest common dominator.
+  if (DT.dominates(A, B))
+    return A;
+
+  /* so the LCA has to be A's parent according to Muchnick */
+  DomTreeNode *IDomA = A->getIDom();
+
+  // Walk NodeB immediate dominators chain and find common dominator node.
+  DomTreeNode *IDomB = B->getIDom();
+  while (IDomB) {
+    if (IDomB == IDomA)
+      return IDomB;
+    Frontiers[IDomB->getBlock()].insert(BB);
+
+    IDomB = IDomB->getIDom();
+  }
+
+  assert(0);
+  return NULL;
+}
+
+void
+PostDominanceFrontier::calculate(const PostDominatorTree &DT, Function &F) {
+  Stype S;
+  constructS(DT, F, S);
+  for (Stype::const_iterator I = S.begin(), E = S.end(); I != E; ++I) {
+    DomTreeNode *mNode = I->first;
+    DomTreeNode *nNode = I->second;
+    findNearestCommonDominator(DT, mNode, nNode);
+  }
+}
+
+#else /* CONTROL_DEPENDENCE_GRAPH */
+
 const DominanceFrontier::DomSetType &
 PostDominanceFrontier::calculate(const PostDominatorTree &DT,
                                  const DomTreeNode *Node) {
@@ -53,3 +113,4 @@ PostDominanceFrontier::calculate(const PostDominatorTree &DT,
 
   return S;
 }
+#endif
--- src/Slicing/PostDominanceFrontier.h
+++ src/Slicing/PostDominanceFrontier.h
@@ -7,10 +7,24 @@
 #ifndef POST_DOMINANCE_FRONTIER
 #define POST_DOMINANCE_FRONTIER
 
+#include "llvm/Analysis/LoopInfo.h"
 #include "llvm/Analysis/DominanceFrontier.h"
 #include "llvm/Analysis/PostDominators.h"
 
 namespace llvm {
+
+  struct CreateHammockCFG : public FunctionPass {
+    static char ID;
+
+    CreateHammockCFG() : FunctionPass(ID) { }
+
+    virtual bool runOnFunction(Function &F);
+
+    virtual void getAnalysisUsage(AnalysisUsage &AU) const {
+      AU.addRequired<LoopInfo>();
+    }
+  };
+
   /// PostDominanceFrontier Class - Concrete subclass of DominanceFrontier that is
   /// used to compute the a post-dominance frontier.
   ///
@@ -19,12 +33,22 @@ namespace llvm {
     PostDominanceFrontier()
       : DominanceFrontierBase(ID, true) { }
 
-    virtual bool runOnFunction(Function &) {
+    virtual bool runOnFunction(Function &F) {
       Frontiers.clear();
       PostDominatorTree &DT = getAnalysis<PostDominatorTree>();
+#ifdef CONTROL_DEPENDENCE_GRAPH
+      calculate(DT, F);
+#else
       Roots = DT.getRoots();
-      if (const DomTreeNode *Root = DT.getRootNode())
+      if (const DomTreeNode *Root = DT.getRootNode()) {
         calculate(DT, Root);
+#ifdef PDF_DUMP
+	errs() << "=== DUMP:\n";
+	dump();
+	errs() << "=== EOD\n";
+#endif
+      }
+#endif
       return false;
     }
 
@@ -34,8 +58,18 @@ namespace llvm {
     }
 
   private:
+#ifdef CONTROL_DEPENDENCE_GRAPH
+    typedef std::pair<DomTreeNode *, DomTreeNode *> Ssubtype;
+    typedef std::set<Ssubtype> Stype;
+
+    void calculate(const PostDominatorTree &DT, Function &F);
+    void constructS(const PostDominatorTree &DT, Function &F, Stype &S);
+    const DomTreeNode *findNearestCommonDominator(const PostDominatorTree &DT,
+		    DomTreeNode *A, DomTreeNode *B);
+#else
     const DomSetType &calculate(const PostDominatorTree &DT,
                                 const DomTreeNode *Node);
+#endif
   };
 }
 
