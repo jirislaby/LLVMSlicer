d3945d939bdb85d9dd8582f947fc5b68884c594a
Jiri Slaby <jirislaby@gmail.com>
2013-05-13 16:24:08 +0200

    pts-to: merge PredefContainers and PointsTo

--- src/PointsTo/PointsTo.cpp
+++ src/PointsTo/PointsTo.cpp
@@ -3,7 +3,10 @@
 
 #include <functional>
 
-#include "PredefContainers.h"
+#include "llvm/BasicBlock.h"
+#include "llvm/Instruction.h"
+#include "llvm/Module.h"
+
 #include "PointsTo.h"
 #include "RuleExpressions.h"
 
@@ -403,6 +406,57 @@ getPointsToSet(const llvm::Value *const &memLoc, const PointsToSets &S) {
     return emptySet;
   }
   return it->second;
+}
+
+ProgramStructure::ProgramStructure(Module &M) : M(M) {
+    typedef llvm::Module::const_global_iterator GlobalsIter;
+    for (GlobalsIter g = M.global_begin(); g != M.global_end(); ++g)
+      if (llvm::isGlobalPointerInitialization(&*g))
+	detail::toRuleCode(&*g,std::back_inserter(this->getContainer()));
+
+    detail::FunctionsMap FM;
+    detail::CallsMap CM;
+    detail::buildCallMaps(M,FM,CM);
+
+    typedef llvm::Module::const_iterator FunctionsIter;
+    for (FunctionsIter f = M.begin(); f != M.end(); ++f) {
+	typedef llvm::Function::const_iterator BasicBlocksIter;
+	for (BasicBlocksIter b = f->begin(); b != f->end(); ++b)
+	{
+	    typedef llvm::BasicBlock::const_iterator InstructionsIter;
+	    for (InstructionsIter i = b->begin(); i != b->end(); ++i)
+		if (llvm::isPointerManipulation(&*i))
+		    detail::toRuleCode(&*i,
+				std::back_inserter(this->getContainer()));
+		else if (llvm::CallInst const* const c =
+			    llvm::dyn_cast<llvm::CallInst>(&*i)) {
+		    if (!isInlineAssembly(c))
+			detail::collectCallRuleCodes(c,
+			    FM.lower_bound(llvm::getCalleePrototype(c)),
+			    FM.upper_bound(llvm::getCalleePrototype(c)),
+			    std::back_inserter(this->getContainer()));
+		} else if (llvm::ReturnInst const* const r =
+			    llvm::dyn_cast<llvm::ReturnInst>(&*i)) {
+		    llvm::FunctionType const* const t =
+			r->getParent()->getParent()->getFunctionType();
+		    detail::collectReturnRuleCodes(r,CM.lower_bound(t),
+			    CM.upper_bound(t),
+			    std::back_inserter(this->getContainer()));
+		}
+	}
+    }
+#ifdef PS_DEBUG
+    errs() << "==PS START\n";
+    for (const_iterator I = getContainer().begin(), E = getContainer().end();
+	    I != E; ++I) {
+	const RuleCode &rc = *I;
+	errs() << "\tTYPE=" << rc.getType() << "\n\tL=";
+	rc.getLvalue()->dump();
+	errs() << "\tR=";
+	rc.getRvalue()->dump();
+    }
+    errs() << "==PS END\n";
+#endif
 }
 
 }}
