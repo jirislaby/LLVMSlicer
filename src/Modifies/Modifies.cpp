b2934535fcd7c97ee0451db44a18fb6cc32c468e
Jiri Slaby <jirislaby@gmail.com>
2013-05-13 16:33:37 +0200

    mods: merge Modifies and PredefContainers

--- src/Modifies/Modifies.cpp
+++ src/Modifies/Modifies.cpp
@@ -5,6 +5,9 @@
 #include <iterator>
 
 #include "llvm/Constant.h"
+#include "llvm/Instruction.h"
+#include "llvm/Instructions.h"
+#include "llvm/Module.h"
 
 #include "../Callgraph/Callgraph.h"
 #include "../PointsTo/PointsTo.h"
@@ -12,6 +15,29 @@
 
 using namespace llvm;
 using namespace llvm::mods;
+
+namespace llvm { namespace mods {
+
+  ProgramStructure::ProgramStructure(Module &M) {
+    for (Module::iterator f = M.begin(); f != M.end(); ++f)
+      if (!f->isDeclaration() && !memoryManStuff(&*f))
+        for (inst_iterator i = inst_begin(*f); i != inst_end(*f); ++i)
+          if (const StoreInst *s = dyn_cast<StoreInst>(&*i)) {
+            const Value *l = elimConstExpr(s->getPointerOperand());
+	    this->getContainer()[&*f].push_back(ProgramStructure::Command(
+		  hasExtraReference(l) ? CMD_VAR : CMD_DREF_VAR, l));
+          }
+  }
+
+  const Modifies::ModSet &getModSet(const llvm::Function *const &f,
+	    const Modifies &S) {
+    static const Modifies::ModSet empty;
+    const Modifies::const_iterator it = S.find(f);
+
+    return (it == S.end()) ? empty : it->second;
+  }
+
+}}
 
 void llvm::mods::computeModifies(const ProgramStructure &P,
       const callgraph::Callgraph &CG, const ptr::PointsToSets &PS,
