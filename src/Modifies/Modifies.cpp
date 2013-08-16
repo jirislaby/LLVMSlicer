72cb1b37020104b29818da9a2799157fb63ff0f6
Jiri Slaby <jirislaby@gmail.com>
2013-08-16 22:07:57 +0200

    mods: little cleanup
    Move computeModifies into rhe llvm::mods block so we do not need to
    prepend it.

--- src/Modifies/Modifies.cpp
+++ src/Modifies/Modifies.cpp
@@ -14,7 +14,6 @@
 #include "Modifies.h"
 
 using namespace llvm;
-using namespace llvm::mods;
 
 namespace llvm { namespace mods {
 
@@ -37,48 +36,50 @@ namespace llvm { namespace mods {
     return (it == S.end()) ? empty : it->second;
   }
 
-}}
 
-void llvm::mods::computeModifies(const ProgramStructure &P,
-      const callgraph::Callgraph &CG, const ptr::PointsToSets &PS,
-      Modifies &MOD) {
+  void computeModifies(const ProgramStructure &P,
+	const callgraph::Callgraph &CG, const ptr::PointsToSets &PS,
+	Modifies &MOD) {
 
-  for (ProgramStructure::const_iterator f = P.begin(); f != P.end(); ++f)
-    for (ProgramStructure::mapped_type::const_iterator c = f->second.begin();
-	 c != f->second.end(); ++c)
-      if (c->getType() == CMD_VAR) {
-	if (!isLocalToFunction(c->getVar(),f->first))
-	    MOD[f->first].insert(c->getVar());
-      } else if (c->getType() == CMD_DREF_VAR) {
-	typedef ptr::PointsToSets::PointsToSet PTSet;
-	const PTSet &S = ptr::getPointsToSet(c->getVar(), PS);
-	for (PTSet::const_iterator p = S.begin(); p != S.end(); ++p)
-	  if (!isLocalToFunction(p->first, f->first) &&
-			  !isConstantValue(p->first))
-	    MOD[f->first].insert(p->first);
-      }
+    for (ProgramStructure::const_iterator f = P.begin(); f != P.end(); ++f)
+      for (ProgramStructure::mapped_type::const_iterator c = f->second.begin();
+	   c != f->second.end(); ++c)
+	if (c->getType() == CMD_VAR) {
+	  if (!isLocalToFunction(c->getVar(), f->first))
+	      MOD[f->first].insert(c->getVar());
+	} else if (c->getType() == CMD_DREF_VAR) {
+	  typedef ptr::PointsToSets::PointsToSet PTSet;
+	  const PTSet &S = ptr::getPointsToSet(c->getVar(), PS);
 
-  typedef callgraph::Callgraph Callgraph;
-  for (Callgraph::const_iterator i = CG.begin_closure();
-	i != CG.end_closure(); ++i) {
-    const Modifies::mapped_type &src = MOD[i->second];
-    typedef Modifies::mapped_type dst_t;
-    dst_t &dst = MOD[i->first];
+	  for (PTSet::const_iterator p = S.begin(); p != S.end(); ++p)
+	    if (!isLocalToFunction(p->first, f->first) &&
+			    !isConstantValue(p->first))
+	      MOD[f->first].insert(p->first);
+	}
 
-    std::copy(src.begin(), src.end(), std::inserter(dst, dst.end()));
+    typedef callgraph::Callgraph Callgraph;
+    for (Callgraph::const_iterator i = CG.begin_closure();
+	  i != CG.end_closure(); ++i) {
+      const Modifies::mapped_type &src = MOD[i->second];
+      typedef Modifies::mapped_type dst_t;
+      dst_t &dst = MOD[i->first];
+
+      std::copy(src.begin(), src.end(), std::inserter(dst, dst.end()));
 #if 0 /* original boost+STL uncompilable crap */
-    using std::tr1::bind;
-    using std::tr1::placeholders::_1;
-    using std::tr1::cref;
-    dst.erase(std::remove_if(dst.begin(), dst.end(),
-	      bind(&ProgramStructure::isLocalToFunction, cref(P), _1, i->first)),
-	      dst.end());
+      using std::tr1::bind;
+      using std::tr1::placeholders::_1;
+      using std::tr1::cref;
+      dst.erase(std::remove_if(dst.begin(), dst.end(),
+		bind(&ProgramStructure::isLocalToFunction, cref(P), _1, i->first)),
+		dst.end());
 #endif
-    for (dst_t::iterator I = dst.begin(), E = dst.end(); I != E; ) {
-      if (isLocalToFunction(*I, i->first))
-	dst.erase(I++);
-      else
-	++I;
+      for (dst_t::iterator I = dst.begin(), E = dst.end(); I != E; ) {
+	if (isLocalToFunction(*I, i->first))
+	  dst.erase(I++);
+	else
+	  ++I;
+      }
     }
   }
-}
+
+}}
