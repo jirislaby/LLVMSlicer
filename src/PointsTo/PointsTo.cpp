jirislaby/LLVMSlicer

commit f775a9705e64b47ab6e77b14bc1174d559c0f017

pts-to: handle VarArg functions properly, take 2
We were still not handling them for return instructions properly. Fix
that the same as for the first case by having return types in the
CallMap, isntead of full types and check whether types are compatible
when it comes to building output iterators.

src/PointsTo/PointsTo.cpp
@@ -21,7 +21,7 @@ class CallMaps {
 private:
   /* return type -> function */
   typedef std::multimap<const Type *, const Function *> FunctionsMap;
-  typedef std::multimap<const FunctionType *, const CallInst *> CallsMap;
+  typedef std::multimap<const Type *, const CallInst *> CallsMap;
 
 public:
   CallMaps(const Module &M) {
@@ -128,15 +128,19 @@ void CallMaps::collectReturnRuleCodes(const ReturnInst *r, OutIterator out) {
     return;
 
   const Function *f = r->getParent()->getParent();
-  const FunctionType *fTy = f->getFunctionType();
+  const FunctionType *funTy = f->getFunctionType();
+  const Type *retTy = funTy->getReturnType();
 
-  for (CallsMap::const_iterator b = CM.lower_bound(fTy),
-      e = CM.upper_bound(fTy); b != e; ++b)
-    if (const Function *g = b->second->getCalledFunction()) {
+  for (CallsMap::const_iterator b = CM.lower_bound(retTy),
+      e = CM.upper_bound(retTy); b != e; ++b) {
+    const CallInst *CI = b->second;
+
+    if (const Function *g = CI->getCalledFunction()) {
       if (f == g)
-	*out++ = argPassRuleCode(b->second, retVal);
-    } else
-      *out++ = argPassRuleCode(b->second, retVal);
+	*out++ = argPassRuleCode(CI, retVal);
+    } else if (compatibleFunTypes(funTy, g->getFunctionType()))
+      *out++ = argPassRuleCode(CI, retVal);
+  }
 }
 
 void CallMaps::buildCallMaps(const Module &M) {
@@ -153,7 +157,7 @@ void CallMaps::buildCallMaps(const Module &M) {
 		if (!isInlineAssembly(CI) && !callToMemoryManStuff(CI)) {
 		    const FunctionType *funTy = getCalleePrototype(CI);
 
-		    CM.insert(std::make_pair(funTy, CI));
+		    CM.insert(std::make_pair(funTy->getReturnType(), CI));
 		}
 	    } else if (const StoreInst *SI = dyn_cast<StoreInst>(&*i)) {
 		const Value *r = SI->getValueOperand();

commit 5a95fb67a661dd2e4bfa2a6e47a1e65cd1a1c52a

pts-to: fix regression in return call maps
The commit f775a9705e64b47ab6e77b14bc1174d559c0f017 (pts-to: handle
VarArg functions properly, take 2) introduced a regression to
CallMaps::collectReturnRuleCodes. We dereference there g, which is
only defined in the other branch. Fix it by checking for proper type.

src/PointsTo/PointsTo.cpp
@@ -154,8 +154,8 @@ void CallMaps::collectReturnRuleCodes(const ReturnInst *r, OutIterator out) {
     if (const Function *g = CI->getCalledFunction()) {
       if (f == g)
 	*out++ = argPassRuleCode(CI, retVal);
-    } else if (compatibleFunTypes(funTy, g->getFunctionType()))
-      *out++ = argPassRuleCode(CI, retVal);
+    } else if (compatibleFunTypes(funTy, getCalleePrototype(CI)))
+	*out++ = argPassRuleCode(CI, retVal);
   }
 }
 